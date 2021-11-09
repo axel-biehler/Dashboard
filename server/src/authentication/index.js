d33aeb96951b1be807fc2da15fd907e502d9d67a
Alexandre Sauner <alexandre.snr@icloud.com>
2021-11-09 16:11:41 +0100

    feat(authentication): add auth server middleware

--- server/src/authentication/index.js
+++ server/src/authentication/index.js
@@ -1,9 +1,11 @@
+const authMiddleware = require('./authMiddleware');
 const generateJwt = require('./generateJwt');
 const hashPassword = require('./hashPassword');
 const usernameAvailable = require('./usernameAvailable');
 const verifyPassword = require('./verifyPassword');
 
 module.exports = {
+  authMiddleware,
   generateJwt,
   hashPassword,
   usernameAvailable,
