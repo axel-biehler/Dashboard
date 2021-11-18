57ecbe5975e8c91ddf41dead21bfe47e163c7540
Alexandre Sauner <alexandre.snr@icloud.com>
2021-11-18 18:30:29 +0100

    feat(widgets): improve epitech widgets

--- server/src/services/epitech/netsoul/route.js
+++ server/src/services/epitech/netsoul/route.js
@@ -1,12 +1,32 @@
 const fetch = require('node-fetch');
 
+const timeConverter = (timestamp) => {
+  const a = new Date(timestamp * 1000);
+  const time = `${a.getDate()}/${a.getMonth()}`;
+  return time;
+};
+
 const route = async (req, res) => {
   try {
     const r = await fetch(`https://intra.epitech.eu/${req.instance.params.autologin}/user?format=json`);
     const body = await r.json();
+
+    const { login } = body;
+
+    const r2 = await fetch(`https://intra.epitech.eu/${req.instance.params.autologin}/user/${login}/netsoul?format=json`);
+    const body2 = await r2.json();
+
+    const data = body2.slice(-7).map((val) => ([
+      timeConverter(val[0] + 3600),
+      Math.round(((val[1] / 60) / 60) * 100) / 100,
+      Math.round(((val[5] / 60) / 60) * 100) / 100,
+    ]));
+
     res.json({
       status: true,
-      gpa: body.nsstat.active,
+      name: body.title,
+      promo: body.promo,
+      netsoul: data,
     });
   } catch (e) {
     console.error(e);
--- webapp/src/components/services/epitech/Netsoul.jsx
+++ webapp/src/components/services/epitech/Netsoul.jsx
@@ -1,17 +1,30 @@
-import { Typography } from '@mui/material';
 import React from 'react';
+import { Typography, Box, Divider } from '@mui/material';
+import { Chart } from 'react-google-charts';
 
 const Netsoul = ({ data }) => (
-  <div>
-    <Typography sx={{ fontSize: 24, mb: 0 }} color="text.secondary" gutterBottom>
-      Your log time
-    </Typography>
-    <Typography variant="h2" component="div">
-      {data.gpa}
+  <Box sx={{ height: '100%' }} display="flex" flexDirection="column">
+    <Typography sx={{ padding: 2, fontSize: 24, mb: 0 }} color="text.secondary" gutterBottom>
+      {data.name}
       {' '}
-      h
+      -
+      {' '}
+      Epitech
+      {' '}
+      {data.promo}
     </Typography>
-  </div>
+    <Divider />
+    <Box p={2} display="flex" flexGrow={1} flexDirection="row" alignItems="center" justifyContent="space-evenly">
+      <Chart
+        chartType="Bar"
+        loader={<div>Loading Chart</div>}
+        data={[
+          ['', 'Active', 'Average'],
+          ...data.netsoul,
+        ]}
+      />
+    </Box>
+  </Box>
 );
 
 export default Netsoul;
